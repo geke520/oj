b58baa47f79c853e4700248662e8f0b9abf61b44
geke <18684706805@163.com>
2021-05-23 17:29:38 +0800

    N27

---
+++ N27.cpp
@@ -0,0 +1,27 @@
+// 给你一个字符串S，要求你将字符串中出现的所有"gzu"子串删除，
+// 输出删除之后的S。
+#include<bits/stdc++.h>
+using namespace std;
+void delete_s(string s)
+{
+    for (int i = 0; i < s.length();i++)
+    {
+        if((s[i]=='g'||s[i]=='G')&&(s[i+1]=='z'||s[i+1]=='Z')&&(s[i+2]=='u'||s[i+2]=='U'))
+        {
+            s[i]=s[i+1]=s[i+2]=-1;
+        }
+    }
+    for (int i = 0; i < s.length();i++)
+    {
+        if(s[i]!=-1)
+        {
+            cout << s[i];
+        }
+    }
+}
+int main()
+{
+    string s;
+    cin >> s;
+    delete_s(s);
+}
